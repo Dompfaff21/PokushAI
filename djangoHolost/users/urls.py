b8983376466243e0fc669652d37e6c46c4fc4988
FuckingPengvin <kutsev.artem@mail.ru>
2024-08-02 22:51:51 +0700

    Настроил ссылки

--- djangoHolost/users/urls.py
+++ djangoHolost/users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('new', views.create_user, name='new'),
+    path('', views.login_user, name='user'),
+    path('new', views.registrate),
 ]
--- djangoHolost/users/views.py
+++ djangoHolost/users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def create_user(request):
+def login_user(request):
+    return render(request, 'user.html')
+
+def registrate(request):
     return render(request, 'reg.html')
