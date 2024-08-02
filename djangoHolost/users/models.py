8ba790190061859aeda0e9600da080ed83c2d2e3
Dompfaff21 <iliakarpov2004@gmail.com>
2024-08-03 03:54:20 +0700

    удалил class в models.py

--- djangoHolost/users/models.py
+++ djangoHolost/users/models.py
@@ -1,8 +1,4 @@
 from django.db import models
 
 # Create your models here.
-class Users(models.Model):
-    name = models.CharField('Имя', max_length=50)
-    family = models.CharField('Фамилия', max_length=50)
-    email = models.CharField('E-mail', max_length=256)
-    number = models.CharField('Номер телефона', max_length=11)
+
