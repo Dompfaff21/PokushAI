86424f7cffe03f525a2e14c04744adb826166201
FuckingPengvin <kutsev.artem@mail.ru>
2024-08-05 20:07:46 +0700

    Проверь теперь, должно работать

--- djangoHolost/accounts/urls.py
+++ djangoHolost/accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login_user, name='accounts'),
+    path('login', views.login_user, name='login'),
     path('new', views.registrate, name='new'),
 ]
