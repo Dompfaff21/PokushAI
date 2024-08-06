da34b09b847129627b9b7e957cb572ddc43d7080
FuckingPengvin <kutsev.artem@mail.ru>
2024-08-07 01:33:58 +0700

    Снова работает регистрация новая, но не работает авторизация

--- djangoHolost/Users/forms.py
+++ djangoHolost/Users/forms.py
@@ -4,15 +4,22 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    phone = forms.CharField(max_length=11, required=True)
+    phone = forms.CharField(max_length=11, required=True, widget=forms.TextInput(attrs={'placeholder':'Введите номер телефона'}))
 
     class Meta:
         model = User
         fields = ('username', 'phone', 'email', 'password1', 'password2',)
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder':'Введите логин'}),
+            'phone': forms.TextInput(attrs={'placeholder': 'Введите номер телефона'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Введите E-mail'}),
+            'password1': forms.PasswordInput(attrs={'placeholder': 'Введите пароль'}),
+            'password2': forms.PasswordInput(attrs={'placeholder': 'Повторите пароль'})
+        }
 
 class LoginForm(AuthenticationForm):
-    name = forms.CharField()
-    password = forms.CharField()
+    name = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Введите логин'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder':'Введите пароль'}))
 
     class Meta:
         model = User
--- djangoHolost/Users/views.py
+++ djangoHolost/Users/views.py
@@ -5,36 +5,29 @@ from .forms import  SignUpForm, LoginForm
 
 
 # Create your views here.
-def register(request):
-    form = SignUpForm(request.POST)
-
-    if form.is_valid():
-        user = form.save()
-        Profile.objects.create(user=user, phone=form.cleaned_data.get('phone'))
-        username = form.cleaned_data.get('username')
-        password = form.cleaned_data.get('password1')
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        return redirect('/')
-    else:
-        return redirect('signup')
-
-def login(request):
-    form = LoginForm(request.POST)
-
-    if form.is_valid():
-        username = form.cleaned_data.get('name')
-        password = form.cleaned_data.get('password')
-        user = authenticate(username=username, password=password)
-        login(request, user)
-        return redirect('/')
-    else:
-        return redirect('signup')
-
-
 def signup(request):
-    context = {
+    if 'reg' in request.POST:
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            Profile.objects.create(user=user, phone=form.cleaned_data.get('phone'))
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/')
+
+    elif 'log' in request.POST:
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('user')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            return redirect('/')
+
+    content = {
         'form': SignUpForm(),
         'form1': LoginForm()
     }
-    return render(request, 'signup.html', context)
+    return render(request, 'signup.html', content)
