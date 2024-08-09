daed3330d088940d934feae5a6945c6d1ed6cec7
Dompfaff21 <iliakarpov2004@gmail.com>
2024-08-10 00:24:35 +0700

    добавил страницу о нас, настроил ссылки

--- djangoHolost/main/urls.py
+++ djangoHolost/main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_page),
+    path('', views.index_page, name='home'),
+    path('about', views.about_page, name='about')
 ]
