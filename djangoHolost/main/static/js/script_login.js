2a510b05f582abceffafc1e0dcb82b6f1d18718f
Dompfaff21 <iliakarpov2004@gmail.com>
2024-08-14 22:35:51 +0700

    удалил пользователей с бдшки, крутое восстановление пароля балдеж уффф

--- djangoHolost/main/static/js/script_login.js
+++ djangoHolost/main/static/js/script_login.js
@@ -14,17 +14,14 @@ loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 
-// При нажатии на кнопку "Зарегистрироваться" добавляем класс "active"
 regBTN.addEventListener('click', () => {
-    localStorage.setItem('containerState', 'active'); // Сохраняем состояние "active"
+    localStorage.setItem('containerState', 'active');
 });
 
-// При нажатии на кнопку "Войти" удаляем класс "active"
 logBTN.addEventListener('click', () => {
-    localStorage.setItem('containerState', ''); // Очищаем состояние
+    localStorage.setItem('containerState', '');
 });
 
-// Восстанавливаем состояние контейнера при загрузке страницы
 window.addEventListener('load', () => {
     const state = localStorage.getItem('containerState');
     if (state === 'active') {
