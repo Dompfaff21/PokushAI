02a8e30f13a06edf2cdcfaf5641d3e4ae8742028
FuckingPengvin <kutsev.artem@mail.ru>
2024-08-14 21:43:59 +0700

    Исправил ошибку после изменения пароля

--- djangoHolost/users/forms.py
+++ djangoHolost/users/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
-from django.contrib.auth import password_validation
 from django.contrib.auth.forms import PasswordResetForm, SetPasswordForm
 
 class SignUpForm(UserCreationForm):
