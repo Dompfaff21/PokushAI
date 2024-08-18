b0202c11bf4b5303f51295691a48c43176fddb3f
FuckingPengvin <kutsev.artem@mail.ru>
2024-08-18 23:05:50 +0700

    Исправил ошибку после изменения пароля

--- djangoHolost/users/urls.py
+++ djangoHolost/users/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
         path('logout', LogoutView.as_view(), name='logout'),
         path('password-reset/', views.CustomPasswordResetViews.as_view(), name='password-reset'),
         path('password_reset_confirm/<uidb64>/<token>/',
-             auth_views.PasswordResetConfirmView.as_view(),
+             views.CustomPasswordResetConfirmViews.as_view(),
              name='password_reset_confirm'),
         path('password_reset_complete/',
         auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'),
