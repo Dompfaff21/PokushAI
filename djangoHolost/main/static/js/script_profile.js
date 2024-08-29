f392150b04f28d5eef6f518e348ccdc3af92b56f
Dompfaff21 <iliakarpov2004@gmail.com>
2024-08-30 02:07:29 +0700

    сделал меню в профиле

---
+++ djangoHolost/main/static/js/script_profile.js
@@ -0,0 +1,54 @@
+// PASSWORD DISPLAY
+
+async function loadSvg(filePath) {
+    const response = await fetch(filePath);
+    const svgText = await response.text();
+    return svgText;
+}
+
+document.addEventListener('DOMContentLoaded', async function() {
+    let svgShowEye = await loadSvg('/static/pictures/dark_theme/eye_dark.svg');
+    let svgHideEye = await loadSvg('/static/pictures/dark_theme/hide_eye_dark.svg');
+    const passwordFields = document.querySelectorAll('input[name="password"], input[name="password1"], input[name="password2"], input[name="new_password1"], input[name="new_password2"]');
+    const eyeIcons = document.querySelectorAll('.eye');
+    passwordFields.forEach((passwordField, index) => {
+        const eyeIcon = eyeIcons[index];
+
+        if (passwordField && eyeIcon) {
+            
+
+            eyeIcon.innerHTML = svgShowEye;   
+            eyeIcon.addEventListener('click', function() {
+                if (passwordField.type === 'password') {
+                    passwordField.type = 'text';
+                    eyeIcon.innerHTML = svgHideEye;
+                } else {
+                    passwordField.type = 'password';
+                    eyeIcon.innerHTML = svgShowEye;
+                }
+            });
+        }    
+    });
+});
+
+// ASIDE
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sidebarItems = document.querySelectorAll('.sidebarp ul li');
+    const contentBoxes = document.querySelectorAll('.content-box');
+
+    sidebarItems.forEach(item => {
+        item.addEventListener('click', function() {
+            sidebarItems.forEach(i => i.classList.remove('active'));
+
+            item.classList.add('active');
+
+            contentBoxes.forEach(box => box.style.display = 'none');
+
+            const target = item.getAttribute('data-target');
+            document.getElementById(target).style.display = 'block';
+        });
+    });
+
+    document.getElementById('personal-info').style.display = 'block';
+});
