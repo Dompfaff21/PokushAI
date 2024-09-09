6932f6afa963fde92f303428f9d6283cb474dec2
FuckingPengvin <kutsev.artem@mail.ru>
2024-09-10 00:08:26 +0700

    Добавление рецептов работает, доступ через recipe/new_post

--- djangoHolost/recipe/urls.py
+++ djangoHolost/recipe/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('posts', views.posts, name='posts')
+    path('posts', views.posts, name='posts'),
+    path('new_post', views.new_post, name='new_post'),
 ]
