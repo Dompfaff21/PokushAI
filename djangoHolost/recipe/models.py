0fa2409777e957131b79c5bca6e6b10a75df3cce
FuckingPengvin <kutsev.artem@mail.ru>
2024-09-09 22:32:20 +0700

    Небольшие правки в БД

--- djangoHolost/recipe/models.py
+++ djangoHolost/recipe/models.py
@@ -5,7 +5,7 @@ class Posts(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField('Заголовок', max_length=160)
     description = models.TextField('Описание', max_length=1000)
-    post_image = models.ImageField(null=True, blank=True)
+    post_image = models.ImageField('Фото к рецепту', null=True, blank=True, upload_to='recipe_pics')
     created_at = models.DateTimeField('Дата создания', auto_now_add=True)
     update_at = models.DateTimeField('Дата последнего обновления', auto_now=True)
 
