96b3255005e4a5d3d7fca48465f733e3f32a3bde
FuckingPengvin <kutsev.artem@mail.ru>
2024-09-24 22:21:09 +0700

    Тестируй

--- djangoHolost/users/views.py
+++ djangoHolost/users/views.py
@@ -193,19 +193,21 @@ def update_post(request, pk):
 
 class RegisterView(APIView):
     def post(self, request):
-        serializer = RegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response({"message": "Пользователь успешно зарегистрирован"}, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if 'reg' in request.POST:
+            serializer = RegisterSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({"message": "Пользователь успешно зарегистрирован"}, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(APIView):
     def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
-            if user:
-                return Response({"message": "Авторизация успешна"})
-            return Response({"error": "Неверные учетные данные"}, status=status.HTTP_401_UNAUTHORIZED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        if 'log' in request.POST:
+            serializer = LoginSerializer(data=request.data)
+            if serializer.is_valid():
+                user = authenticate(username=serializer.data['username'], password=serializer.data['password'])
+                if user:
+                    return Response({"message": "Авторизация успешна"})
+                return Response({"error": "Неверные учетные данные"}, status=status.HTTP_401_UNAUTHORIZED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
