c2da78e21807650ed856eebfdccc4a0b5183f75c
FuckingPengvin <kutsev.artem@mail.ru>
2024-09-24 22:34:17 +0700

    Еще раз

--- djangoHolost/djangoHolost/urls.py
+++ djangoHolost/djangoHolost/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from users.views import LoginView, RegisterView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('users/', include('users.urls')),
-    path('recipe/', include('recipe.urls'))
+    path('recipe/', include('recipe.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
 
 handler404 = 'main.views.error_404_view'
--- djangoHolost/users/urls.py
+++ djangoHolost/users/urls.py
@@ -14,7 +14,5 @@ urlpatterns = [
         path('delete_post/<int:id>', views.delete_post, name='delete_post'),
         path('edit_post/<int:id>', views.edit_post, name='edit_post'),
         path('update_post/<pk>', views.update_post),
-        path('login', RegisterView.as_view(), name='register'),
-        path('login', LoginView.as_view(), name='login'),
 
 ]
