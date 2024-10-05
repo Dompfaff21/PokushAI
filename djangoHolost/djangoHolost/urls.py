dbfde14153dd94639501542d273b195cc1149890
FuckingPengvin <kutsev.artem@mail.ru>
2024-10-06 00:20:41 +0700

    Жопа отвалится, если заработает

--- djangoHolost/djangoHolost/urls.py
+++ djangoHolost/djangoHolost/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from users.views import LoginView, RegisterView
+from users.views import LoginView, RegisterView, UserProfileView
 
 
 urlpatterns = [
@@ -10,6 +10,7 @@ urlpatterns = [
     path('recipe/', include('recipe.urls')),
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('media', UserProfileView.as_view())
 ]
 
 handler404 = 'main.views.error_404_view'
