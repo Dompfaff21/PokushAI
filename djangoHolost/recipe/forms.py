7484fa62ff27d2795d5d815219db133a28bb8093
FuckingPengvin <kutsev.artem@mail.ru>
2024-10-08 00:37:35 +0700

    Победа

--- djangoHolost/recipe/forms.py
+++ djangoHolost/recipe/forms.py
@@ -1,10 +1,12 @@
 from django import forms
-from .models import Posts, Steps
 from django.forms import inlineformset_factory
+
+from .models import Posts, Steps
+
 
 class RecipeForm(forms.ModelForm):
     class Meta:
         model = Posts
         fields = ['title', 'description', 'post_image']
 
-StepFormSet = inlineformset_factory(Posts, Steps, fields=['step_des', 'step_image'], extra=1, can_delete=True)
+StepFormSet = inlineformset_factory(Posts, Steps, fields=['step_des', 'step_image'], extra=0, can_delete=True)
--- djangoHolost/recipe/views.py
+++ djangoHolost/recipe/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, redirect
 from .models import Posts, Steps
 from users.models import Profile
 from django.contrib import messages
-from .forms import RecipeForm, StepFormSet
+from .forms import RecipeForm
 
 
 def posts(request):
@@ -19,22 +19,28 @@ def posts(request):
 def new_post(request):
     if request.method == 'POST':
         recipe_form = RecipeForm(request.POST, request.FILES)
-        formset = StepFormSet(request.POST, request.FILES)
-        if recipe_form.is_valid() and formset.is_valid():
+        if recipe_form.is_valid():
             recipe = recipe_form.save(commit=False)
             recipe.author = request.user
             recipe.save()
 
-            steps = formset.save(commit=False)
-            for step in steps:
-                step.recipe = recipe
-                step.save()
+            total_forms = int(request.POST.get('steps-TOTAL_FORMS'))
+
+            for i in range(total_forms):
+                step_description = request.POST.get(f'steps-{i}-step_des')
+                step_image = request.FILES.get(f'steps-{i}-step_image')
+
+                if step_description:
+                    Steps.objects.create(
+                        recipe=recipe,
+                        step_des=step_description,
+                        step_image=step_image,
+                    )
 
             messages.success(request, 'Рецепт успешно создан!')
             return redirect('posts')
 
     else:
         recipe_form = RecipeForm()
-        formset = StepFormSet()
 
-    return render(request, 'new_post.html', {'recipe_form': recipe_form, 'formset': formset})
+    return render(request, 'new_post.html', {'recipe_form': recipe_form})
