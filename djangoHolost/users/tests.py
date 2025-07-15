6e1dda62224f23c2f3eac67c68b05dc969ddfe8a
Pengvin <kutsev.artem@mail.ru>
2025-07-15 18:08:53 +0700

    Написал тест для модели Users и Profile

--- djangoHolost/users/tests.py
+++ djangoHolost/users/tests.py
@@ -6,6 +6,29 @@ from rest_framework import status
 
 from users.models import Profile
 
+class TestModelProfile(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username='testuser1',
+            password='user_test_pass',
+            email='test@mail.com'
+        )
+        cls.profile = Profile.objects.create(
+            user=cls.user,
+            phone='+7 (000)-000-00-00'
+        )
+
+    def test_profile_creation(self):
+        self.assertEquals(Profile.objects.count(), 1)
+        self.assertEquals(self.user.profile, self.profile)
+
+    def tset_one_to_one_relationship(self):
+        with self.assertRaises(Exception):
+            Profile.objects.create(user=self.user, phone='+7 (000)-000-00-00')
+
+    def test_profile_fields(self):
+        self.assertEquals(self.profile.phone, '+7 (000)-000-00-00')
 
 class LoginUserViewTest(APITestCase):
     def setUp(self):
